6dc74a3cb58deb845fdf133936ec2e1b8ecbf434
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-04-24 17:08:18 -0700

    added new view for pop gesture scaling tests

---
+++ PopExperiments/ViewController_View2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController_View2.h
+//  PopExperiments
+//
+//  Created by Jialiang Xiang on 2015-04-24.
+//  Copyright (c) 2015 Jialiang Xiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController_View2 : UIViewController
+
+@end
---
+++ PopExperiments/ViewController_View2.m
@@ -0,0 +1,38 @@
+//
+//  ViewController_View2.m
+//  PopExperiments
+//
+//  Created by Jialiang Xiang on 2015-04-24.
+//  Copyright (c) 2015 Jialiang Xiang. All rights reserved.
+//
+
+#import "ViewController_View2.h"
+
+@interface ViewController_View2 ()
+
+@end
+
+@implementation ViewController_View2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"view2 loaded by controller");
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
