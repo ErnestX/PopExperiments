e4f9468a380fc11c49ac3e0ea8c2c85ef899807f
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-04-30 18:45:58 -0700

    implemented pan handler

--- PopExperiments/View2.m
+++ PopExperiments/View2.m
@@ -11,6 +11,8 @@
 @implementation View2
 {
     CALayer* whiteLayer;
+    CALayer* blueLayer;
+    CGPoint previousLocation;
 }
 
 /*
@@ -24,21 +26,58 @@
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
-    whiteLayer = [CALayer layer];
-    whiteLayer.backgroundColor = [UIColor whiteColor].CGColor;
-    whiteLayer.bounds = CGRectMake(0, 0, 200, 400);
-    whiteLayer.position = CGPointMake(180, 300);
-    [self.layer addSublayer:whiteLayer];
     
-    UIPanGestureRecognizer* panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-    [self addGestureRecognizer:panRecognizer];
+    [self customInit:self];
     
     return self;
 }
 
+- (void) customInit: (UIView*) uv
+{
+    whiteLayer = [CALayer layer];
+    whiteLayer.backgroundColor = [UIColor whiteColor].CGColor;
+    whiteLayer.bounds = CGRectMake(0, 0, 200, 400);
+    whiteLayer.position = CGPointMake(180, 300);
+    [uv.layer addSublayer:whiteLayer];
+    
+    blueLayer = [CALayer layer];
+    blueLayer.backgroundColor = [UIColor colorWithRed:0.1 green:0.6 blue:1 alpha:1].CGColor;
+    blueLayer.bounds = CGRectMake(0, 0, 50, 50);
+    blueLayer.position = CGPointMake(50, 55);
+    [whiteLayer addSublayer:blueLayer];
+    
+    UIPanGestureRecognizer* panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:uv action:@selector(handlePan:)];
+    [uv addGestureRecognizer:panRecognizer];
+}
+
 - (void) handlePan:(UIPanGestureRecognizer*) uigr
 {
-    
+    switch (uigr.state) {
+        case UIGestureRecognizerStateBegan:
+        {
+            NSLog(@"touch began");
+            // Remember original location
+            previousLocation = blueLayer.position;
+        }
+            
+        case UIGestureRecognizerStateChanged:
+        {
+            //lastScrollSpeed = [uigr velocityInView:self];
+            CGPoint translation = [uigr translationInView:self]; // pan up or scroll down = negative
+            CGPoint newPosition = CGPointMake(previousLocation.x + translation.x, previousLocation.y + translation.y);
+            [CATransaction begin];
+            [CATransaction setDisableActions:YES];
+            blueLayer.position = newPosition;
+            [CATransaction commit];
+        }
+            
+        case UIGestureRecognizerStateEnded:
+        {
+            // NSLog(@"touch ended");
+            // start animation with lastScrollSpeed as initial speed
+        }
+    }
+
 }
 
 @end
